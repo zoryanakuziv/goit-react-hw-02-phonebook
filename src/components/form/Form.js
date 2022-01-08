c0f27bf249f7a4460fd503bbfc7ec60ebd52d67e
Zoryana Kuziv <zoryana.kuziv@gmail.com>
2022-01-08 14:30:18 +0100

    correct some errors

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -32,27 +32,27 @@ export default class Form extends Component {
     const { name, number } = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
-        <Label htmlFor={this.nameInputId}>
+        <Label >
           Name
           <FormInput
             type="text"
             value={name}
             onChange={this.handleChange}
-            id={this.nameInputId}
+            
             name="name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
         </Label>
-        <Label htmlFor={this.numberInputId}>
+        <Label >
           Number
           <FormInput
             type="tel"
             name="number"
             value={number}
             onChange={this.handleChange}
-            id={this.numberInputId}
+          
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
